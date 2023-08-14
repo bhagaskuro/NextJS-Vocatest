5cd6af31ed1c9c8f8573cf4a00aa4935b659ee77
Bhagaskoro P. Nindyalit <kurohakuro@gmail.com>
2023-08-14 17:45:34 +0700

    fix: minor update

--- voca/middleware.js
+++ voca/middleware.js
@@ -3,26 +3,19 @@ import { NextResponse } from "next/server";
 export default function middleware(req) {
   let verify = req.cookies.get("loggedIn");
   let url = req.url;
+  const uri = "https://vocatest-delta.vercel.app/";
 
   if (!verify && url.includes("/profile")) {
-    return NextResponse.redirect(
-      "https://vocatest-qnvjgylmd-bhagaskuro.vercel.app/login"
-    );
+    return NextResponse.redirect(uri + "/login");
   }
   if (!verify && url.includes("/edit")) {
-    return NextResponse.redirect(
-      "https://vocatest-qnvjgylmd-bhagaskuro.vercel.app/login"
-    );
+    return NextResponse.redirect(uri + "/login");
   }
   if (!verify && url.includes("/change-password")) {
-    return NextResponse.redirect(
-      "https://vocatest-qnvjgylmd-bhagaskuro.vercel.app/login"
-    );
+    return NextResponse.redirect(uri + "/login");
   }
 
   if (verify && url.includes("/login")) {
-    return NextResponse.redirect(
-      "https://vocatest-qnvjgylmd-bhagaskuro.vercel.app/profile"
-    );
+    return NextResponse.redirect(uri + "/profile");
   }
 }
